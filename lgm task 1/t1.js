f2b9fb81abcbd0285013728fb8481f2bf6236959
Thirutamizh <160699211+Thirutamizh@users.noreply.github.com>
2024-02-21 11:11:19 +0530

    Add files via upload

---
+++ lgm task 1/t1.js
@@ -0,0 +1,24 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+
+    if (taskInput.value.trim() === '') {
+        alert('Please enter a task!');
+        return;
+    }
+
+    const li = document.createElement('li');
+    li.innerHTML = `
+        ${taskInput.value}
+        <button onclick="removeTask(this)">Remove</button>
+    `;
+
+    taskList.appendChild(li);
+    taskInput.value = '';
+}
+
+function removeTask(button) {
+    const li = button.parentNode;
+    const taskList = document.getElementById('taskList');
+    taskList.removeChild(li);
+}
